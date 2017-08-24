46a212946acc479cec7d5f7cc2bc3aff4bf408a1
Antoine Martin <antoine97.martin@gmail.com>
2017-08-24 15:55:33 +0200

    Add basic caddy middleware setup
    Can be tested by importing txtdirect in caddymain/run.go
    and adding "txtdirect" to the directives of the httpserver package.
    
    Then build caddy, and run it in the directory of this Caddyfile:
    
    ```
    localhost
    
    txtdirect
    ```

---
+++ txtdirect.go
@@ -0,0 +1,33 @@
+package txtdirect
+
+import (
+	"net/http"
+
+	"github.com/mholt/caddy"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+)
+
+func init() {
+	caddy.RegisterPlugin("txtdirect", caddy.Plugin{
+		ServerType: "http",
+		Action:     setup,
+	})
+}
+
+func setup(c *caddy.Controller) error {
+	cfg := httpserver.GetConfig(c)
+	mid := func(next httpserver.Handler) httpserver.Handler {
+		return Handler{Next: next}
+	}
+	cfg.AddMiddleware(mid)
+	return nil
+}
+
+type Handler struct {
+	Next httpserver.Handler
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	w.Write([]byte("Hello TxtDirect !"))
+	return 200, nil
+}

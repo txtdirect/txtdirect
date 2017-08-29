0325a72ccf6899701c4efa875b15c85fa76417d4
Antoine Martin <antoine97.martin@gmail.com>
2017-08-29 22:44:18 +0200

    Add tests for gometa
    Fix error detected while running tests

--- gometa.go
+++ gometa.go
@@ -7,16 +7,16 @@ import (
 
 var tmpl = template.Must(template.New("").Parse(`<!DOCTYPE html>
 <head>
-<meta name="go-import" content="{{.Host}}{{.Path}} {{.Vcs}} {{.NewUrl}}">
+<meta name="go-import" content="{{.Host}}{{.Path}} {{.Vcs}} {{.NewURL}}">
 </head>
 </html>`))
 
 func gometa(w http.ResponseWriter, r record, host, path string) error {
 	return tmpl.Execute(w, struct {
-		Host string
-		Path string
-		Vcs  string
-		URL  string
+		Host   string
+		Path   string
+		Vcs    string
+		NewURL string
 	}{
 		host,
 		path,

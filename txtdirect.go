ffa2d6b9456f0d8280048e0bef7610736a6fd0a9
David Riesz <dbriesz@users.noreply.github.com>
2018-05-29 15:17:31 -0700

    Change DNS TXT lookup to use the absolute zone

--- txtdirect.go
+++ txtdirect.go
@@ -103,7 +103,13 @@ func getBaseTarget(rec record) (string, int) {
 
 func getRecord(host, path string) (record, error) {
 	zone := strings.Join([]string{basezone, host}, ".")
-	s, err := net.LookupTXT(zone)
+	var absoluteZone string
+	if strings.HasSuffix(zone, ".") {
+		absoluteZone = zone
+	} else {
+		absoluteZone = strings.Join([]string{zone, "."}, "")
+	}
+	s, err := net.LookupTXT(absoluteZone)
 	if err != nil {
 		return record{}, fmt.Errorf("could not get TXT record: %s", err)
 	}

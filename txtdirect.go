66f989401f2e86dbf247854e4679d04e0f20170b
Erfan Besharat <erbesharat@gmail.com>
2018-08-09 16:52:38 +0430

    Add more placeholders

--- txtdirect.go
+++ txtdirect.go
@@ -19,6 +19,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
+	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -123,16 +125,78 @@ func getBaseTarget(rec record, r *http.Request) (string, int) {
 	return rec.To, rec.Code
 }
 
-func parsePlaceholders(url string, r *http.Request) string {
-	placeholders := PlaceholderRegex.FindAllStringSubmatch(url, -1)
+func parsePlaceholders(input string, r *http.Request) string {
+	placeholders := PlaceholderRegex.FindAllStringSubmatch(input, -1)
 	for _, placeholder := range placeholders {
 		switch placeholder[0] {
 		case "{uri}":
 			uri := []rune(r.URL.RequestURI())
-			url = strings.Replace(url, "{uri}", string(uri[1:]), -1)
+			input = strings.Replace(input, "{uri}", string(uri[1:]), -1)
+		case "{dir}":
+			dir, _ := path.Split(r.URL.Path)
+			input = strings.Replace(input, "{dir}", dir, -1)
+		case "{file}":
+			_, file := path.Split(r.URL.Path)
+			input = strings.Replace(input, "{file}", file, -1)
+		case "{fragment}":
+			input = strings.Replace(input, "{fragment}", r.URL.Fragment, -1)
+		case "{host}":
+			input = strings.Replace(input, "{host}", r.URL.Host, -1)
+		case "{hostonly}":
+			host, _, err := net.SplitHostPort(r.Host)
+			if err != nil {
+				input = strings.Replace(input, "{hostonly}", r.Host, -1)
+				continue
+			}
+			input = strings.Replace(input, "{hostonly}", host, -1)
+		case "{method}":
+			input = strings.Replace(input, "{method}", r.Method, -1)
+		case "{path}":
+			input = strings.Replace(input, "{path}", r.URL.Path[1:], -1)
+		case "{path_escaped}":
+			input = strings.Replace(input, "{path_escaped}", url.QueryEscape(r.URL.Path), -1)
+		case "{port}":
+			_, port, err := net.SplitHostPort(r.Host)
+			if err != nil {
+				input = strings.Replace(input, "{port}", "", -1)
+				continue
+			}
+			input = strings.Replace(input, "{port}", port, -1)
+		case "{query}":
+			input = strings.Replace(input, "{query}", r.URL.RawQuery, -1)
+		case "{query_escaped}":
+			input = strings.Replace(input, "{query_escaped}", url.QueryEscape(r.URL.RawQuery), -1)
+		case "{uri_escaped}":
+			input = strings.Replace(input, "{uri_escaped}", url.QueryEscape(r.URL.RequestURI()), -1)
+		case "{user}":
+			user, _, ok := r.BasicAuth()
+			if !ok {
+				input = strings.Replace(input, "{user}", "", -1)
+			}
+			input = strings.Replace(input, "{user}", user, -1)
+		}
+		if placeholder[0][1] == '>' {
+			want := placeholder[0][2 : len(placeholder[0])-1]
+			for key, values := range r.Header {
+				// Header placeholders (case-insensitive)
+				if strings.EqualFold(key, want) {
+					input = strings.Replace(input, placeholder[0], strings.Join(values, ","), -1)
+				}
+			}
+		}
+		if placeholder[0][1] == '~' {
+			name := placeholder[0][2 : len(placeholder[0])-1]
+			if cookie, err := r.Cookie(name); err == nil {
+				input = strings.Replace(input, placeholder[0], cookie.Value, -1)
+			}
+		}
+		if placeholder[0][1] == '?' {
+			query := r.URL.Query()
+			name := placeholder[0][2 : len(placeholder[0])-1]
+			input = strings.Replace(input, placeholder[0], query.Get(name), -1)
 		}
 	}
-	return url
+	return input
 }
 
 func getRecord(host, path string, ctx context.Context, c Config) (record, error) {

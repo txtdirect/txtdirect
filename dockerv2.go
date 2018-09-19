052ec427dcb42cedebc5594c7621789c950080c5
Erfan Besharat <erbesharat@gmail.com>
2018-09-19 17:26:14 +0430

    Use predefined regexes to parse path in dockerv2 type

--- dockerv2.go
+++ dockerv2.go
@@ -1,6 +1,35 @@
 package txtdirect
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var dockerRegexs = map[string]string{
+	"_catalog":  "^/v2/_catalog$",
+	"tags":      "^/v2/(.*)/tags/(.*)",
+	"manifests": "^/v2/(.*)/manifests/(.*)",
+	"blobs":     "^/v2/(.*)/blobs/(.*)",
+}
+
+var DockerRegex = regexp.MustCompile("^\\/v2\\/(.*\\/(tags|manifests|blobs)\\/.*|_catalog$)")
+
 func generateDockerv2URI(path string, rec record) (string, int) {
-	// TODO: parse path in future to support all docker apis
+	if path != "" {
+		regexType := DockerRegex.FindAllStringSubmatch(path, -1)[0]
+		pathRegex, err := regexp.Compile(dockerRegexs[regexType[len(regexType)-1]])
+		if err != nil {
+			panic(err)
+		}
+		pathSubmatches := pathRegex.FindAllStringSubmatch(path, -1)
+		pathSlice := pathSubmatches[0][1:]
+
+		uri := rec.To
+		for i, v := range pathSlice {
+			uri = strings.Replace(uri, fmt.Sprintf("$%d", i+1), v, -1)
+		}
+		return uri, rec.Code
+	}
 	return rec.To, rec.Code
 }
--- dockerv2_test.go
+++ dockerv2_test.go
@@ -18,6 +18,35 @@ func Test_generateDockerv2URI(t *testing.T) {
 			},
 			"docker.io/seetheprogress/txtdirect:latest",
 		},
+		{
+			"/v2/testing/tags/docker",
+			record{
+				To:   "https://gcr.io/v2/my_bucket/$1/tags/$2",
+				Code: 302,
+			},
+			"https://gcr.io/v2/my_bucket/testing/tags/docker",
+		},
+		{
+			"/v2/testing/manifests/docker",
+			record{
+				To: "https://gcr.io/v2/my_bucket/$1/manifests/$2",
+			},
+			"https://gcr.io/v2/my_bucket/testing/manifests/docker",
+		},
+		{
+			"/v2/testing/blobs/docker",
+			record{
+				To: "https://gcr.io/v2/my_bucket/$1/blobs/$2",
+			},
+			"https://gcr.io/v2/my_bucket/testing/blobs/docker",
+		},
+		{
+			"/v2/_catalog",
+			record{
+				To: "https://gcr.io/v2/_catalog",
+			},
+			"https://gcr.io/v2/_catalog",
+		},
 	}
 	for _, test := range tests {
 		uri, _ := generateDockerv2URI(test.path, test.rec)

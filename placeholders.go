89dd6c745cfa444b4196915eb1cdd28acd54e62b
David Riesz <dbriesz@users.noreply.github.com>
2018-10-10 13:55:36 -0700

    Separate checks for error and number in if statement

--- placeholders.go
+++ placeholders.go
@@ -48,17 +48,17 @@ func parsePlaceholders(input string, r *http.Request) string {
 			}
 			input = strings.Replace(input, "{user}", user, -1)
 		}
-		/* For multi-level tlds such as example.co.uk, "co" is treated as 
+		/* For multi-level tlds such as example.co.uk, "co" is treated as
 		a subzone from a DNS perspective and would be used as {label1} */
 		if strings.HasPrefix(placeholder[0], "{label") {
 			nStr := placeholder[0][6 : len(placeholder[0])-1] // get the integer N in "{labelN}"
 			n, err := strconv.Atoi(nStr)
-			if err != nil || n < 1 {
-				if err == nil {
-					log.Print("{label0} is not supported")
-				} else {
-					log.Print(err)
-				}
+			if err != nil {
+				log.Print(err)
+				input = strings.Replace(input, placeholder[0], "", -1)
+			}
+			if n < 1 {
+				log.Print("{label0} is not supported")
 				input = strings.Replace(input, placeholder[0], "", -1)
 			}
 			labels := strings.Split(r.URL.Hostname(), ".")

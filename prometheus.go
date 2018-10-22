8a04a840b5e9f1faf815f97c9fcf32e3742a7dc3
Erfan Besharat <erbesharat@gmail.com>
2018-10-22 08:46:56 +0330

    Add Prometheus counters

--- prometheus.go
+++ prometheus.go
@@ -1,5 +1,12 @@
 package txtdirect
 
+import (
+	"log"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
 // Prometheus contains Prometheus's configuration
 type Prometheus struct {
 	Enable   bool
@@ -7,4 +14,25 @@ type Prometheus struct {
 	Path     string
 	Serve    string
 	Hostname string
+}
+
+var (
+	RequestsCount = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "txtdirect",
+		Name:      "total_requests_per_host",
+		Help:      "Total requests per host",
+	}, []string{"host"})
+	RequestsByStatus = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "txtdirect",
+		Name:      "total_returned_statuses_per_host",
+		Help:      "Total requests per host",
+	}, []string{"host", "status"})
+)
+
+func InitPrometheus() {
+	err := prometheus.Register(RequestsCount)
+	err = prometheus.Register(RequestsByStatus)
+	if err != nil {
+		log.Printf("<%s> [txtdirect]: Prometheus registration error: %s", time.Now().Format(logFormat), err.Error())
+	}
 }

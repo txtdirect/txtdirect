2f4fd82b9606d23647cd679aa22f042df1ca6f89
Erfan Besharat <erbesharat@gmail.com>
2018-12-24 18:42:16 +0330

    Add go-source meta tag support to gometa

--- gometa.go
+++ gometa.go
@@ -24,6 +24,7 @@ var tmpl = template.Must(template.New("").Parse(`<!DOCTYPE html>
 <html>
 <head>
 <meta name="go-import" content="{{.Host}}{{.Path}} {{.Vcs}} {{.NewURL}}">
+<meta name="go-source" content="{{.Host}}{{.Path}} _ {{.NewURL}}/tree/v2{/dir} {{.NewURL}}/blob/v2{/dir}/{file}#L{line}">
 </head>
 </html>`))
 

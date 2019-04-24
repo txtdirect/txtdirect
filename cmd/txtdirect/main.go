41a822afb7a3884560a3830baec1bf93d69be96c
Michael Grosser <development@stp-ip.net>
2019-04-24 21:34:02 +0000

    Fix whitespace

--- cmd/txtdirect/main.go
+++ cmd/txtdirect/main.go
@@ -15,7 +15,7 @@ package main
 
 import (
 	"github.com/mholt/caddy/caddy/caddymain"
-	
+
 	_ "github.com/SchumacherFM/mailout"
 	_ "github.com/captncraig/caddy-realip"
 	_ "github.com/miekg/caddy-prometheus"

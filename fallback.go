8931d5750c1c86a577a99db5b037c365f1d82071
Erfan Besharat <erbesharat@gmail.com>
2019-08-02 10:34:55 +0430

    (fallback): Add fallback struct

--- fallback.go
+++ fallback.go
@@ -9,71 +9,75 @@ import (
 	"strings"
 )
 
+type Fallback struct {
+	rw      http.ResponseWriter
+	request *http.Request
+	config  Config
+
+	records    []record
+	pathRecord record
+
+	fallbackType string
+	code         int
+}
+
 // fallback redirects the request to the given fallback address
 // and if it's not provided it will check txtdirect config for
 // default fallback address
-func fallback(w http.ResponseWriter, r *http.Request, fallback, recordType, fallbackType string, code int, c Config) {
+func fallback(w http.ResponseWriter, r *http.Request, fallbackType string, code int, c Config) {
 	if code == http.StatusMovedPermanently {
 		w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%d", status301CacheAge))
 	}
 	w.Header().Add("Status-Code", strconv.Itoa(code))
 
+	f := Fallback{
+		rw:           w,
+		request:      r,
+		config:       c,
+		fallbackType: fallbackType,
+		code:         code,
+	}
+
 	if fallbackType != "global" {
 
 		// Fetch records from request's context and set the []record type on them
-		records := r.Context().Value("records").([]record)
+		f.fetchRecords()
 
 		// Redirect to first record's `to=` field
-		if fallbackType == "to" && records[0].To != "" {
-			http.Redirect(w, r, records[0].To, code)
-			if c.Prometheus.Enable {
-				countFallback(r, records[0].Type, fallbackType, code)
-			}
+		if fallbackType == "to" && f.records[0].To != "" {
+			http.Redirect(w, r, f.records[0].To, code)
+			f.countFallback(f.records[0].Type)
 		}
 
 		// Redirect to first record's `website=` field
-		if fallbackType == "website" && records[0].Website != "" {
-			http.Redirect(w, r, records[0].Website, code)
-			if c.Prometheus.Enable {
-				countFallback(r, records[0].Type, fallbackType, code)
-			}
+		if fallbackType == "website" && f.records[0].Website != "" {
+			http.Redirect(w, r, f.records[0].Website, code)
+			f.countFallback(f.records[0].Type)
 		}
 
 		// Dockerv2 root fallback
-		if fallbackType == "root" && records[0].Root != "" {
-			http.Redirect(w, r, records[0].Root, code)
-			if c.Prometheus.Enable {
-				countFallback(r, records[len(records)-1].Type, fallbackType, code)
-			}
+		if fallbackType == "root" && f.records[0].Root != "" {
+			http.Redirect(w, r, f.records[0].Root, code)
+			f.countFallback(f.records[0].Type)
 		}
 
-		// Special case when path is used in fetching the final record
-		var pathRecord record
-		if len(records) >= 2 {
-			pathRecord = records[len(records)-2]
+		if fallbackType == "root" && f.pathRecord.Root != "" {
+			http.Redirect(w, r, f.pathRecord.Root, code)
+			f.countFallback(f.pathRecord.Type)
 		}
 
-		if fallbackType == "root" && pathRecord.Root != "" {
-			http.Redirect(w, r, pathRecord.Root, code)
-			if c.Prometheus.Enable {
-				countFallback(r, records[len(records)-1].Type, fallbackType, code)
-			}
-		}
-
-		if pathRecord.To != "" {
-			http.Redirect(w, r, pathRecord.To, code)
-			if c.Prometheus.Enable {
-				countFallback(r, records[len(records)-1].Type, fallbackType, code)
-			}
+		if f.pathRecord.To != "" {
+			http.Redirect(w, r, f.pathRecord.To, code)
+			f.countFallback(f.pathRecord.Type)
 		}
 
 		// If non of the above cases applied on the record, jump into global redirects
-		globalFallbacks(w, r, c, code, records[len(records)-1].Type)
+		f.globalFallbacks(f.records[len(f.records)-1].Type)
 		log.Printf("[txtdirect]: %s > %s", r.Host+r.URL.Path, w.Header().Get("Location"))
 		return
 	}
 
-	globalFallbacks(w, r, c, code, "")
+	f.globalFallbacks("")
 
 	log.Printf("[txtdirect]: %s > %s", r.Host+r.URL.Path, w.Header().Get("Location"))
 }
@@ -93,27 +97,36 @@ func addRecordToContext(r *http.Request, rec record) *http.Request {
 	return r.WithContext(context.WithValue(r.Context(), "records", records))
 }
 
-func countFallback(r *http.Request, recType, fallbackType string, code int) {
-	FallbacksCount.WithLabelValues(r.Host, recType, fallbackType).Add(1)
-	RequestsByStatus.WithLabelValues(r.URL.Host, string(code)).Add(1)
+func (f *Fallback) countFallback(recType string) {
+	if f.config.Prometheus.Enable {
+		FallbacksCount.WithLabelValues(f.request.Host, recType, f.fallbackType).Add(1)
+		RequestsByStatus.WithLabelValues(f.request.URL.Host, string(f.code)).Add(1)
+	}
 }
 
-func globalFallbacks(w http.ResponseWriter, r *http.Request, c Config, code int, recordType string) {
-	if contains(c.Enable, "www") {
-		s := strings.Join([]string{defaultProtocol, "://", defaultSub, ".", r.URL.Host}, "")
-		http.Redirect(w, r, s, code)
-		if c.Prometheus.Enable {
-			countFallback(r, recordType, "subdomain", code)
-		}
-	} else if c.Redirect != "" {
-		w.Header().Set("Status-Code", strconv.Itoa(http.StatusMovedPermanently))
+func (f *Fallback) globalFallbacks(recordType string) {
+	if contains(f.config.Enable, "www") {
+		s := strings.Join([]string{defaultProtocol, "://", defaultSub, ".", f.request.URL.Host}, "")
 
-		http.Redirect(w, r, c.Redirect, http.StatusMovedPermanently)
+		http.Redirect(f.rw, f.request, s, f.code)
 
-		if c.Prometheus.Enable {
-			countFallback(r, recordType, "redirect", http.StatusMovedPermanently)
-		}
+		f.countFallback(recordType)
+	} else if f.config.Redirect != "" {
+		f.rw.Header().Set("Status-Code", strconv.Itoa(http.StatusMovedPermanently))
+
+		http.Redirect(f.rw, f.request, f.config.Redirect, http.StatusMovedPermanently)
+
+		f.code = http.StatusMovedPermanently
+
+		f.countFallback(recordType)
 	} else {
-		http.NotFound(w, r)
+		http.NotFound(f.rw, f.request)
+	}
+}
+
+func (f *Fallback) fetchRecords() {
+	f.records = f.request.Context().Value("records").([]record)
+	if len(f.records) >= 2 {
+		f.pathRecord = f.records[len(f.records)-2]
 	}
 }

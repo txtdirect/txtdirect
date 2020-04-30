a11aadcdf8e2788b8e54bfd659c252262d4c88b7
Erfan Besharat <erbesharat@gmail.com>
2020-04-30 18:11:53 +0430

    (dockerv2): Add unit tests for new parser
    Signed-off-by: Erfan Besharat <erbesharat@gmail.com>

--- dockerv2_test.go
+++ dockerv2_test.go
@@ -15,6 +15,7 @@ package txtdirect
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
@@ -121,6 +122,158 @@ func Test_generateDockerv2URI(t *testing.T) {
 		}
 		if !strings.Contains(resp.Body.String(), test.expected) {
 			t.Errorf("Expected %s, got %s:", test.expected, resp.Body.String())
+		}
+	}
+}
+
+func TestDockerv2_ParseRecordReference(t *testing.T) {
+	tests := []struct {
+		rec      record
+		expected Image
+	}{
+		{
+			rec: record{
+				To: "https://example.com",
+			},
+			expected: Image{
+				Registry: "https://example.com",
+			},
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect/txtdirect",
+			},
+			expected: Image{
+				Registry: "https://example.com",
+				Image:    "txtdirect/txtdirect",
+			},
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect",
+			},
+			expected: Image{
+				Registry: "https://example.com",
+				Image:    "txtdirect",
+			},
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect/txtdirect:v0.0.1",
+			},
+			expected: Image{
+				Registry: "https://example.com",
+				Image:    "txtdirect/txtdirect",
+				Tag:      "v0.0.1",
+			},
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect:v0.0.1",
+			},
+			expected: Image{
+				Registry: "https://example.com",
+				Image:    "txtdirect",
+				Tag:      "v0.0.1",
+			},
+		},
+	}
+	for _, test := range tests {
+		resp := httptest.NewRecorder()
+		docker := NewDockerv2(resp, &http.Request{}, test.rec, Config{})
+
+		if err := docker.ParseRecordReference(); err != nil {
+			t.Errorf("Unexpected error happened: %s", err)
+		}
+		if docker.image != test.expected {
+			t.Errorf("Expected %+v, got %+v", test.expected, docker.image)
+		}
+	}
+}
+
+func TestDockerv2_ParseReference(t *testing.T) {
+	tests := []struct {
+		rec      record
+		uri      string
+		expected string
+	}{
+		{
+			rec: record{
+				To: "https://example.com",
+			},
+			uri:      "https://example.test/v2/txtdirect/manifests/latest",
+			expected: "https://example.com/v2/txtdirect/manifests/latest",
+		},
+		{
+			rec: record{
+				To: "https://example.com",
+			},
+			uri:      "https://example.test/v2/txtdirect/manifests/v0.0.1",
+			expected: "https://example.com/v2/txtdirect/manifests/v0.0.1",
+		},
+		{
+			rec: record{
+				To: "https://example.com",
+			},
+			uri:      "https://example.test/v2/txtdirect/blobs/sha256:000000",
+			expected: "https://example.com/v2/txtdirect/blobs/sha256:000000",
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect/txtdirect",
+			},
+			uri:      "https://example.test/v2/txtdirect/txtdirect/manifests/latest",
+			expected: "https://example.com/v2/txtdirect/txtdirect/manifests/latest",
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect/txtdirect",
+			},
+			uri:      "https://example.test/v2/txtdirect/txtdirect/blobs/sha256:000000",
+			expected: "https://example.com/v2/txtdirect/txtdirect/blobs/sha256:000000",
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect",
+			},
+			uri:      "https://example.test/v2/txtdirect/txtdirect/manifests/latest",
+			expected: "https://example.com/v2/txtdirect/manifests/latest",
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect",
+			},
+			uri:      "https://example.test/v2/txtdirect/txtdirect/blobs/sha256:000000",
+			expected: "https://example.com/v2/txtdirect/blobs/sha256:000000",
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect/txtdirect:v0.0.1",
+			},
+			uri:      "https://example.test/v2/txtdirect/manifests/latest",
+			expected: "https://example.com/v2/txtdirect/txtdirect/manifests/v0.0.1",
+		},
+		{
+			rec: record{
+				To: "https://example.com/txtdirect:v0.0.1",
+			},
+			uri:      "https://example.test/v2/txtdirect/txtdirect/blobs/sha256:000000",
+			expected: "https://example.com/v2/txtdirect/blobs/sha256:000000",
+		},
+	}
+	for _, test := range tests {
+		req := httptest.NewRequest("GET", test.uri, nil)
+		resp := httptest.NewRecorder()
+		docker := NewDockerv2(resp, req, test.rec, Config{})
+
+		uri, err := docker.ParseReference()
+
+		if err != nil {
+			t.Errorf("Unexpected error happened: %s", err)
+		}
+
+		if uri != test.expected {
+			t.Errorf("Expected %s for dockerv2 redirect URI, got %s", test.expected, uri)
 		}
 	}
 }
